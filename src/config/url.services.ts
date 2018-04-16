42239e9dbf6d2f7bf1770ba9ae4da422f41e0d6d
ALAN <tiesto.alansanchez@gmail.com>
2018-04-16 10:56:58 -0500

    Url externa servicios

--- src/config/url.services.ts
+++ src/config/url.services.ts
@@ -1,5 +1,5 @@
 //export const URL_TRACKER_SERVICE="http://tracker.inpronet.inprotelecom.mx:8087/trackerinpro/rest/mobile";
-export const URL_TRACKER_SERVICE="http://192.168.100.5:8080/tracker-cr/rest/mobile";
+export const URL_TRACKER_SERVICE="http://187.188.193.211:7078/tracker/rest/mobile";
 export const LOGIN="/login";
 export const PROJECT_SUBPROJECT="/loadProjectSubprojects";
 export const CASES="/loadCases";
@@ -11,4 +11,3 @@ export const SAVE_WI_ELEMENT="/saveWiElement";
 export const ETYPE_CONFIG_WI_STATUS="/loadEtypeConfigWiStatus";
 export const ETYPE_CONFIG_DOCS="/loadEtypeConfigDocs";
 export const SAVE_FILE="/saveFile";
-
